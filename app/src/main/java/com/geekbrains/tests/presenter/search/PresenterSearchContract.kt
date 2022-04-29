38ce19db1debe586cf374ff1ba1cf13d1b73b540
pfrost982 <pfrost982@gmail.com>
2022-04-29 20:14:47 +0700

    initial commit

---
+++ app/src/main/java/com/geekbrains/tests/presenter/search/PresenterSearchContract.kt
@@ -0,0 +1,9 @@
+package com.geekbrains.tests.presenter.search
+
+import com.geekbrains.tests.presenter.PresenterContract
+
+interface PresenterSearchContract : PresenterContract {
+    fun searchGitHub(searchQuery: String)
+    //onAttach
+    //onDetach
+}
