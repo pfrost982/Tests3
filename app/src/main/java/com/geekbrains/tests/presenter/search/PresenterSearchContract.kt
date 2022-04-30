705b4345aea8275cc1f00760bbbc85e4079b814b
pfrost982 <pfrost982@gmail.com>
2022-05-01 00:53:59 +0700

    добавлены onAttach, onDetach

--- app/src/main/java/com/geekbrains/tests/presenter/search/PresenterSearchContract.kt
+++ app/src/main/java/com/geekbrains/tests/presenter/search/PresenterSearchContract.kt
@@ -1,9 +1,10 @@
 package com.geekbrains.tests.presenter.search
 
 import com.geekbrains.tests.presenter.PresenterContract
+import com.geekbrains.tests.view.search.ViewSearchContract
 
 interface PresenterSearchContract : PresenterContract {
     fun searchGitHub(searchQuery: String)
-    //onAttach
-    //onDetach
+    fun onAttach(view: ViewSearchContract)
+    fun onDetach(view: ViewSearchContract)
 }
